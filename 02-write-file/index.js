37c8bc8933ee785e692e284ee0550237f50e2d1e
Lidia Bereza <l.bereza365@gmail.com>
2023-05-09 19:20:26 +0200

    feat: add alternative solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,26 @@
-const { stdin, stdout} = process;
-let fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const readline = require('node:readline');
+const {
+    stdin: input,
+    stdout: output,
+} = require('node:process');
 let newFile = fs.createWriteStream(`${__dirname}\\text.txt`);
 
-stdout.write('Введите текст для сохранения?\n');
-stdin.on('data', data => {
-    let text = data.toString();
-    if(text.trim() === 'exit') process.exit();
-    newFile.write(text);
+const RL = readline.createInterface({ input, output });
+
+RL.question('Введите текст для сохранения... \n', (data) => {
+    if (data === 'exit') closeRL();
+    newFile.write(data + '\n');
+    RL.on('line', (text) => {newFile.write(text + '\n')});
 });
-process.on( "SIGINT", function() {
-    process.exit();
-});
-process.on('exit', () => stdout.write(`\n\n   Вы восхитительны!
-    ☆*･｡*.･★*･｡*.･☆   ^     ^
-                  乁( =•ㅅ •=乁) \n`));
+RL.on("SIGINT", () => {closeRL()});
+
+function closeRL() {
+    console.log(
+        `\n\n   Вы восхитительны!
+   ☆*･｡*.･★*･｡*.･☆   ^     ^
+                 乁( =•ㅅ •=乁) \n`
+    )
+    RL.close();
+}
